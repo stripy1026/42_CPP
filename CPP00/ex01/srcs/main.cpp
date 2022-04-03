13043d6ab081077b13b6ac5806609c3e8ab13f42
stripy1026 <inwoo1026@naver.com>
2022-04-03 15:53:00 +0900

    done

--- CPP00/ex01/srcs/main.cpp
+++ CPP00/ex01/srcs/main.cpp
@@ -1,11 +1,27 @@
 #include "PhoneBook.hpp"
+#include "color.hpp"
+
+#define ERR_WCOMMAND "Error : Use Valid command : ADD, SEARCH, EXIT"
 
 int main(void)
 {
     PhoneBook pb;
+    std::string cmd;
+    bool exit = false;
 
     pb.print_startup_message();
-    pb.add_contact();
-    pb.display_contact();
+    while (!exit)
+    {
+        std::cout << GREEN << "Command : " << RESET;
+        std::getline(std::cin, cmd);
+        if (cmd == "ADD")
+            pb.add_contact();
+        else if (cmd == "SEARCH")
+            pb.display_contact();
+        else if (cmd == "EXIT")
+            exit = true;
+        else
+            std::cout << RED << ERR_WCOMMAND << RESET << std::endl;
+    }
     return (0);
 }
