7a0fbee84b81f4cc453649f61d4b08132e8e7878
stripy1026 <aequatio1026@gmail.com>
2022-04-08 01:37:39 +0900

    cpp01 ex02 done

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -1,4 +1,21 @@
-int	main()
+#include <iostream>
+#include <string>
+
+int main()
 {
-	return (0);
+    std::string str("HI THIS IS BRAIN");
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout << "[ **** The memory address **** ]" << std::endl;
+    std::cout << "of the string variable : " << &str << std::endl;
+    std::cout << "held by stringPTR : " << stringPTR << std::endl;
+    std::cout << "held by stringREF : " << &stringREF << std::endl;
+
+    std::cout << "[ **** The value **** ]" << std::endl;
+    std::cout << "of the string variable : " << str << std::endl;
+    std::cout << "pointed to by stringPTR : " << *stringPTR << std::endl;
+    std::cout << "pointed to by stringREF : " << stringREF << std::endl;
+
+    return (0);
 }
