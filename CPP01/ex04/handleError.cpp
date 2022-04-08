1d038da65cca4ba0efceccf7bc091d62e06992c2
stripy1026 <aequatio1026@gmail.com>
2022-04-08 16:33:45 +0900

    std::cerr

--- CPP01/ex04/handleError.cpp
+++ CPP01/ex04/handleError.cpp
@@ -2,6 +2,6 @@
 
 int handleError(std::string message, int value)
 {
-    std::cout << BRIGHT_RED << "Error : " << RESET << message << std::endl;
+    std::cerr << BRIGHT_RED << "Error : " << RESET << message << std::endl;
     return (value);
 }
