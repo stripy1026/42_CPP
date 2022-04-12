0f5c898fb9af2b4e572637e0b514d66e97cade07
stripy1026 <aequatio1026@gmail.com>
2022-04-13 00:23:41 +0900

    cpp02 ex00 done

--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -1,17 +1,24 @@
 #ifndef _FIXED_HPP_
 #define _FIXED_HPP_
 
+#include <iostream>
+
+#include "color.hpp"
+
 class Fixed
 {
   private:
-    int m_fixed_point_number;
-    static const int number_of_fractional_bits;
+    int m_raw_bits;
+    static const int m_bnp_pos;
 
   public:
     Fixed();
     ~Fixed();
-    Fixed(const Fixed &rhs);
+    Fixed(const Fixed &src);
     Fixed &operator=(const Fixed &rhs);
-}
+
+    int getRawBits() const;
+    void setRawBits(const int raw_bits);
+};
 
 #endif // _FIXED_HPP_
