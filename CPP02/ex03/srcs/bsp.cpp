e6d70c64ce140bb44960dc0d441c558f2f1bf2d9
stripy1026 <aequatio1026@gmail.com>
2022-04-14 01:36:37 +0900

    ex03 done

--- CPP02/ex03/srcs/bsp.cpp
+++ CPP02/ex03/srcs/bsp.cpp
@@ -1,5 +1,28 @@
 #include "Point.hpp"
 
+Fixed calPartition(const Point &p, const Point &a, const Point &b)
+{
+	Fixed Px = p.getX();
+	Fixed Py = p.getY();
+	Fixed Ax = a.getX();
+	Fixed Ay = a.getY();
+	Fixed Bx = b.getX();
+	Fixed By = b.getY();
+	Fixed ret;
+
+	ret = (Px - Bx) * (Ay - By) - (Py - By) * (Ax - Bx);
+	return (ret);
+}
+
 bool bsp(const Point a, const Point b, const Point c, const Point point)
 {
+	Fixed p1;
+	Fixed p2;
+	Fixed p3;
+
+	p1 = calPartition(point, a, b);
+	p2 = calPartition(point, b, c);
+	p3 = calPartition(point, c, a);
+
+	return ((p1 < 0 && p2 < 0 && p3 < 0) || (p1 > 0 && p2 > 0 && p3 > 0));
 }
--- CPP02/ex03/srcs/main.cpp
+++ CPP02/ex03/srcs/main.cpp
@@ -1,18 +1,22 @@
-#include "Fixed.hpp"
+#include "Point.hpp"
 
 int main()
 {
-    Fixed a;
-    Fixed const b(Fixed(5.05f) * Fixed(2));
+    Point a(1, 2);
+    Point b(1, 6);
+    Point c(9, 2);
+    Point p1(2.1f, 3.1f);
+    Point p2(0, 3);
+    Point p3(3, 2);
+    Point p4(5, 4);
+    Point p5(4, 4);
 
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-
-    std::cout << b << std::endl;
-    std::cout << Fixed::max(a, b) << std::endl;
+    std::cout << "a " << a << ", b " << b << ", c " << c << std::endl;
+    std::cout << "p1 " << p1 << " is " << (bsp(a, b, c, p1) ? "inside" : "outside") << std::endl;
+    std::cout << "p2 " << p2 << " is " << (bsp(a, b, c, p2) ? "inside" : "outside") << std::endl;
+    std::cout << "p3 " << p3 << " is " << (bsp(a, b, c, p3) ? "inside" : "outside") << std::endl;
+    std::cout << "p4 " << p4 << " is " << (bsp(a, b, c, p4) ? "inside" : "outside") << std::endl;
+    std::cout << "p5 " << p5 << " is " << (bsp(a, b, c, p5) ? "inside" : "outside") << std::endl;
 
     return (0);
 }
