f2cdaa1f6fd93122902a9140812f669b32d4d65c
stripy1026 <aequatio1026@gmail.com>
2022-05-28 22:03:36 +0900

    feat: :sparkles: CPP05 ex00 exception throw need

--- CPP05/ex00/includes/Bureaucrat.hpp
+++ CPP05/ex00/includes/Bureaucrat.hpp
@@ -1,4 +1,33 @@
 #ifndef _BUREAUCRAT_HPP_
 #define _BUREAUCRAT_HPP_
 
+#include <exception>
+#include <iostream>
+
+#include "color.hpp"
+
+class Bureaucrat
+{
+  private:
+    const std::string m_name;
+    const int m_grade;
+
+    Bureaucrat();
+
+  public:
+    ~Bureaucrat();
+    Bureaucrat(const Bureaucrat &src);
+    Bureaucrat &operator=(const Bureaucrat &rhs);
+
+    Bureaucrat(const std::string &name, const int grade);
+
+    const std::string &getName() const;
+    const int &getGrade() const;
+
+    void incrementGrade(const int num);
+    void decrementGrade(const int num);
+};
+
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &bureaucrat);
+
 #endif // _BUREAUCRAT_HPP_
--- CPP05/ex00/srcs/Bureaucrat.cpp
+++ CPP05/ex00/srcs/Bureaucrat.cpp
@@ -1,1 +1,52 @@
 #include "Bureaucrat.hpp"
+
+Bureaucrat()
+{
+}
+
+~Bureaucrat()
+{
+}
+
+Bureaucrat(const Bureaucrat &src) : m_name(src.m_name), m_grade(src.m_grade)
+{
+}
+
+Bureaucrat &operator=(const Bureaucrat &rhs)
+{
+    if (this == &rhs)
+        return (*this);
+    m_name = rhs.m_name;
+    m_grade = rhs.m_grade;
+    return (*this);
+}
+
+Bureaucrat(const std::string &name, const int grade) : m_name(name), m_grade(grade)
+{
+}
+
+const std::string &getName() const
+{
+    return (m_name);
+}
+
+const int &getGrade() const
+{
+    return (m_grade);
+}
+
+void incrementGrade(const int num)
+{
+    m_grade -= num;
+}
+
+void decrementGrade(const int num)
+{
+    m_grade += num;
+}
+
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &bureaucrat)
+{
+    o << GREEN << bureaucrat.getName() << RESET ", bureaucrat grade " GREEN << bureaucrat.getGrade() << RESET;
+    return (o);
+}
