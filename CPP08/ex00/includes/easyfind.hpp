511ca6059cab9ac64795c023b84d175fe9773458
stripy <stripy1026.com>
2022-06-08 14:03:06 +0900

    style: :art: divide hpp and tpp

--- CPP08/ex00/includes/easyfind.hpp
+++ CPP08/ex00/includes/easyfind.hpp
@@ -1,4 +1,8 @@
 #ifndef _EASYFIND_HPP_
 #define _EASYFIND_HPP_
 
+template <typename T> void easyfind(const T &container, const int find);
+
+#include "easyfind.tpp"
+
 #endif // _EASYFIND_HPP_
---
+++ CPP08/ex00/includes/easyfind.tpp
@@ -0,0 +1,9 @@
+#ifdef _EASYFIND_HPP_
+
+template <typename T> void easyfind(const T &container, const int find)
+{
+    (void)container;
+    (void)find;
+}
+
+#endif // _EASYFIND_HPP_
